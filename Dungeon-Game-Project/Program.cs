f625c8ef8bb166bdb0a49da6146e91b0141dbc6d
Burak Baskir <baskir@etsu.edu>
2025-10-08 00:08:40 -0400

    Dungeon-Game-Project Added

---
+++ Dungeon-Game-Project/Program.cs
@@ -0,0 +1,114 @@
+﻿using System;
+using System.Threading; // Thread.Sleep için gerekli
+
+class Program
+{
+    static Random random = new Random();
+
+    static int PlayerDamageAndEnemyHealth(int EnemyHealth)
+    {
+        Console.WriteLine("Click 'Enter' to attack");
+        Console.ReadKey();
+        int PlayerDamage = random.Next(12, 16);
+        Console.WriteLine($@"You Hit {PlayerDamage} Damage
+It must Hurt!");
+        EnemyHealth -= PlayerDamage;
+        Console.WriteLine($"Monster Health is {EnemyHealth}");
+        return EnemyHealth;
+    }
+
+    static int EnemyDamageAndPlayerHealth(int PlayerHealth)
+    {
+        int EnemyDamage = random.Next(6, 9);
+        Console.WriteLine($"You receive {EnemyDamage} Damage");
+        PlayerHealth -= EnemyDamage;
+        Console.WriteLine($"Player Health is {PlayerHealth}");
+        return PlayerHealth;
+    }
+
+    static void FightLoop()
+    {
+        int playerHealth = 30;
+        int EnemyHealth = 30;
+        Console.WriteLine("The Fight Starts!");
+
+        while (true)
+        {
+            EnemyHealth = PlayerDamageAndEnemyHealth(EnemyHealth);
+            if (EnemyHealth <= 0)
+            {
+                Console.WriteLine("Great Job! You killed the monster!");
+                break;
+            }
+
+            Console.WriteLine("Monster Attacks!");
+            Thread.Sleep(2000);
+            playerHealth = EnemyDamageAndPlayerHealth(playerHealth);
+
+            if (playerHealth <= 0)
+            {
+                Console.WriteLine("DEFEAT! Monster Killed you!");
+                Console.WriteLine("Do you want to try again? enter (y/n)");
+                string choice = Console.ReadLine();
+                if (choice == "y")
+                {
+                    Main(null); // tekrar oyuna dön
+                }
+                else
+                {
+                    Environment.Exit(0); // oyundan çık
+                }
+            }
+        }
+    }
+
+    static void Main(string[] args)
+    {
+        Console.WriteLine(@"Hey Wake Up! 
+We are lucky he didnt eat us yet we have to find a way to escape from here!
+Hide Hide! he is coming for us");
+        Console.WriteLine("\n Click enter to hide");
+        Console.ReadKey();
+        Console.WriteLine("Please please not me! AAAAAAAAAAAAAA");
+        Console.WriteLine("Your mate is gone and he is gonna come for you as well find a way to get out");
+        Console.WriteLine("\n Click enter to walk thru dungeon");
+        Console.ReadKey();
+        Console.WriteLine("There is a separation left and right, select one");
+
+        string WayChoice = Console.ReadLine().ToLower();
+
+        while (true)
+        {
+            if (WayChoice == "right")
+            {
+                Console.WriteLine("Here is a door try to open it");
+                Console.WriteLine("Click enter to try");
+                Console.ReadKey();
+                Console.WriteLine("It is locked, try other way");
+                Console.WriteLine("Click enter to go left side");
+                Console.ReadKey();
+                WayChoice = "left"; // tekrar sola yönlendir
+            }
+            else if (WayChoice == "left")
+            {
+                Console.WriteLine("You encounter to the monster! you better fight for your life");
+                FightLoop();
+                Console.WriteLine("You found a key on monster, try it to open the door");
+                break;
+            }
+            else
+            {
+                Console.WriteLine("Invalid Way Choice. Type right or left:");
+                WayChoice = Console.ReadLine().ToLower();
+            }
+        }
+
+        Console.WriteLine("Click enter to go to the door");
+        Console.ReadKey();
+        Console.WriteLine("To try open door click enter");
+        Console.ReadKey();
+        Console.WriteLine("It Works!! You could escape from the dungeon");
+
+        Environment.Exit(0); // ÇIKIŞ (return yazmana gerek yok)
+    }
+}
---
+++ Dungeon-Game-Project/README.md
@@ -0,0 +1,99 @@
+# Dungeon Escape Game
+
+A **C#** console application created as part of the **CSCI 1250** course. This is an interactive dungeon escape game where the player must make choices and fight monsters to escape successfully.
+
+---
+
+## 🚀 Features
+
+* Interactive dungeon exploration with decision-making.
+* Real-time combat system with randomized damage.
+* Player and enemy health tracking.
+* Option to retry upon defeat.
+* Multiple game endings based on player choices.
+
+---
+
+## 💾 Installation
+
+Clone the main repository:
+
+`git clone https://github.com/BurakBaskir/CSCI-1250-Projects.git`
+
+Navigate to this project folder inside the repo:
+
+`cd CSCI-1250-Projects/DungeonEscape_Project`
+
+1.  Open the solution file in **Visual Studio** (or any C# IDE).
+2.  Build and run the program.
+
+---
+
+## 🕹️ Usage
+
+Follow the prompts in the console to navigate the dungeon.
+* Choose directions by typing **"left"** or **"right"**.
+* Press **Enter** to perform actions like hiding, walking, attacking, or opening doors.
+
+### Example Flow:
+
+Hey Wake Up!
+We are lucky he didnt eat us yet we have to find a way to escape from here!
+Hide Hide! he is coming for us
+
+Click enter to hide
+Please please not me! AAAAAAAAAAAAAA
+Your mate is gone and he is gonna come for you as well find a way to get out
+
+Click enter to walk thru dungeon
+There is a separation left and right, select one
+right
+Here is a door try to open it
+Click enter to try
+It is locked, try other way
+Click enter to go left side
+You encounter to the monster! you better fight for your life
+The Fight Starts!
+
+Click 'Enter' to attack
+You Hit 13 Damage
+It must Hurt!
+Monster Health is 17
+Monster Attacks!
+You receive 7 Damage
+Player Health is 23
+
+Click 'Enter' to attack
+You Hit 15 Damage
+It must Hurt!
+Monster Health is 2
+Monster Attacks!
+You receive 8 Damage
+Player Health is 15
+
+Click 'Enter' to attack
+You Hit 14 Damage
+It must Hurt!
+Monster Health is -12
+Great Job! You killed the monster!
+You found a key on monster, try it to open the door
+
+Click enter to go to the doorTo try open door click enter
+It Works!! You could escape from the dungeon
+
+
+---
+
+## 📜 Reference / Inspiration
+
+This project was inspired by the **CSCI 1250** class resources provided by Professor Ryan Haas.
+
+* **CSCI 1250 Class Resources:** <https://github.com/CSCI-1250/class_resources_public>
+
+---
+
+## 👨‍💻 Author
+
+**Burak Baskir**
+
+* **GitHub Profile:** <https://github.com/BurakBaskir>
